45fe2facc6d48c160d09bb1690b07bb1fe6c05cd
WisMaja <137833204+WisMaja@users.noreply.github.com>
2024-06-15 16:15:27 +0200

    Pierwszy commit

---
+++ encrypt_decrypt.py
@@ -0,0 +1,41 @@
+from cryptography.fernet import Fernet
+import os
+
+def generate_key():
+    return Fernet.generate_key()
+
+def load_key(key_path):
+    with open(key_path, 'rb') as key_file:
+        return key_file.read()
+
+def save_key(key, key_path):
+    with open(key_path, 'wb') as key_file:
+        key_file.write(key)
+
+def encrypt_file(file_path, key):
+    fernet = Fernet(key)
+    with open(file_path, 'rb') as file:
+        file_data = file.read()
+    encrypted_data = fernet.encrypt(file_data)
+    with open(file_path, 'wb') as file:
+        file.write(encrypted_data)
+
+def decrypt_file(file_path, key):
+    fernet = Fernet(key)
+    with open(file_path, 'rb') as file:
+        encrypted_data = file.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    with open(file_path, 'wb') as file:
+        file.write(decrypted_data)
+
+def encrypt_folder(folder_path, key):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            encrypt_file(file_path, key)
+
+def decrypt_folder(folder_path, key):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            decrypt_file(file_path, key)
